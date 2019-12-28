6525b782900107a1fe1eac506c6eb8370d402f0e
nesterone <igornester@gmail.com>
2019-12-28 19:29:22 +0200

    zorko-designer-31 firebase.config.js:3-5: move keys to env vars I assume... - introduce dotenv-safe

--- firebase.config.js
+++ firebase.config.js
@@ -1,15 +1,10 @@
 export const firebaseConfig = {
-  /**
-   * @todo #30:30m/DEV move keys to env vars
-   *  I assume that keys provided by deployment env, we no need keys
-   *  for dev environment committed in VCS
-   */
-  apiKey: "AIzaSyC3Qe5w0yirGOtlMstveFjdVbxB-vbR-tY",
-  authDomain: "zorko-52d02.firebaseapp.com",
-  databaseURL: "https://zorko-52d02.firebaseio.com",
-  projectId: "zorko-52d02",
-  storageBucket: "zorko-52d02.appspot.com",
-  messagingSenderId: "478402515777",
-  appId: "1:478402515777:web:05b494941d905306e1c7c0",
-  measurementId: "G-W17DNHKJCJ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBSE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGIN_SENFER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASYREMENT_ID
 };
