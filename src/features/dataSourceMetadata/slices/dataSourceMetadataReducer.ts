83c73e28bc737f2bdf7db4bea661a1274cfc0dff
nesterone <igornester@gmail.com>
2020-01-08 19:50:22 +0200

    zorko-designer-120 dataSourceMetadataReducer.ts:14-15: DataSources Migrate... - wrap reducer with unit test

--- src/features/dataSourceMetadata/slices/dataSourceMetadataReducer.ts
+++ src/features/dataSourceMetadata/slices/dataSourceMetadataReducer.ts
@@ -6,12 +6,6 @@ import {
 import {DataSourceMetadataPresenter, DataSourceMetadataState} from '../presenters';
 import {createReducerWithPresenter} from '../../../packages/presenterReducerUtils/createReducerWithPresenter';
 
-/**
- * @todo #114:40m/DEV DataSources Migrate to Presenter-Reducer approach
- *  prep presenters, use createReducer util
- *
- */
-
 export default produce(
   createReducerWithPresenter<DataSourceMetadataState, DataSourceMetadataPresenter>(
     DataSourceMetadataPresenter.create,
