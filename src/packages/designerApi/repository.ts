bf31106dc641104fefe4317fd767956a7c02532b
nesterone <igornester@gmail.com>
2020-01-11 15:29:47 +0200

    zorko-designer-56 config.yml:2-3: Integrate with CI with trigger for push... - switch expamples source

--- src/packages/designerApi/repository.ts
+++ src/packages/designerApi/repository.ts
@@ -1,6 +1,5 @@
 import _ from 'lodash';
-
-const examplesCollection = require('../../../public/spec/vega-lite/index.json');
+import examplesCollection from '../../../public/spec/vega-lite/index.json';
 
 export async function fetchExamplesRepositories() {
   function toResources(items) {
